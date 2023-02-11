6b5c28a1b7616decc2b0034871ff267042dae43e
ethantrac <etrac@ualberta.ca>
2023-02-11 03:05:27 -0700

    Class Update
    Add actor and player class

---
+++ app/src/main/java/com/example/qr_go/Actor.java
@@ -0,0 +1,14 @@
+// unique android device:
+// https://stackoverflow.com/questions/2785485/is-there-a-unique-android-device-id
+
+package com.example.qr_go;
+
+
+public class Actor {
+    private String username;
+    private String androidID;
+
+    public Actor(String username) {
+        this.username = username;
+    }
+}
---
+++ app/src/main/java/com/example/qr_go/Player.java
@@ -0,0 +1,15 @@
+package com.example.qr_go;
+
+public class Player extends Actor {
+    // private ArrayList<QR> qrList;
+
+
+    public Player(String username) {
+        super(username);
+    }
+
+    //
+    public void hashQR(String qrContents) {
+
+    }
+}
