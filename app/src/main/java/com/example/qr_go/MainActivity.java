a7869e42ee8fc4cd0c4253ce75d8a4d7ab6697b0
tracualberta <etrac@ualberta.ca>
2023-03-07 13:17:55 -0700

    Update fragment

--- app/src/main/java/com/example/qr_go/MainActivity.java
+++ app/src/main/java/com/example/qr_go/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.qr_go;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.example.qr_go.PlayerProfile.PlayerProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- app/src/main/java/com/example/qr_go/PlayerProfile/PlayerProfileFragment.java
+++ app/src/main/java/com/example/qr_go/PlayerProfile/PlayerProfileFragment.java
@@ -2,73 +2,83 @@ package com.example.qr_go.PlayerProfile;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.qr_go.Actor.Player;
+import com.example.qr_go.QR;
 import com.example.qr_go.R;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PlayerProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import android.provider.Settings.Secure;
+
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
 public class PlayerProfileFragment extends Fragment {
 
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView usernameTextView;
+    private Button qrButton;
+    private TextView totalScoreTextView;
+    private TextView totalScannedTextView;
+    private String android_id = Secure.getString(getContext().getContentResolver(), Secure.ANDROID_ID);
+    private Player player;
 
     public PlayerProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlayerProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PlayerProfileFragment newInstance(String param1, String param2) {
-        PlayerProfileFragment fragment = new PlayerProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
         // get views from fragment
+        usernameTextView = getView().findViewById(R.id.username);
+        qrButton = getView().findViewById(R.id.my_qr_codes);
+        totalScoreTextView = getView().findViewById(R.id.total_score);
+        totalScannedTextView = getView().findViewById(R.id.total_scanned);
 
-
+        /**
         // get database information
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference collectionReference = db.collection(Player.class.getSimpleName());
+        DocumentReference documentReference = db.collection(Player.class.getSimpleName()).document(android_id);
 
-        //
+        // put data into class
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                player = new Player((String)value.get("username"), (String)value.get("deviceID"), (ArrayList<QR>) value.get("qrList"),
+                        (int)value.get("rank"), (int)value.get("highestScore"), (int)value.get("lowestScore"), (int)value.get("totalScore"));
+            }
+        });
+
+        // update UI
+        usernameTextView.setText(player.getUsername());
+        totalScannedTextView.setText(player.getTotalScore());
+        totalScannedTextView.setText(player.getTotalQR());
+        */
+
+
+        player = new Player("test123", "123", new ArrayList<QR>(), 1, 123, 0, 12345);
+
+        usernameTextView.setText(player.getUsername());
+        totalScannedTextView.setText(player.getTotalScore());
+        totalScannedTextView.setText(player.getTotalQR());
+
     }
 
     @Override
