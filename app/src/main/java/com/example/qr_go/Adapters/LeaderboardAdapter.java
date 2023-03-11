eaab3f432cacfa06e351952231ee4ac0a3c76402
momo3404 <momo.albachachi@gmail.com>
2023-03-11 13:57:42 -0700

    Added comments

--- app/src/main/java/com/example/qr_go/Adapters/LeaderboardAdapter.java
+++ app/src/main/java/com/example/qr_go/Adapters/LeaderboardAdapter.java
@@ -1,6 +1,7 @@
 package com.example.qr_go.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,14 +11,23 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.qr_go.Activities.Profile.PlayerProfileViewActivity;
+import com.example.qr_go.Activities.Profile.ProfileActivity;
+import com.example.qr_go.Activities.Profile.ProfileQRListViewActivity;
+import com.example.qr_go.Actor.Player;
 import com.example.qr_go.Content.LeaderboardContent;
+import com.example.qr_go.QR.QR;
 import com.example.qr_go.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class LeaderboardAdapter extends ArrayAdapter<LeaderboardContent> {
+public class LeaderboardAdapter extends ArrayAdapter<LeaderboardContent>{
 
     Context mContext;
     LayoutInflater inflater;
@@ -38,26 +48,44 @@ public class LeaderboardAdapter extends ArrayAdapter<LeaderboardContent> {
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View view;
-        if (convertView == null){
+        if (convertView == null) {
             view = LayoutInflater.from(getContext()).inflate(R.layout.content_leaderboard, parent, false);
+        } else {
+            view = convertView;
         }
-        else{
-            view= convertView;
-        }
 
-        LeaderboardContent leaderboardContent = getItem(position);
-        TextView playerName = view.findViewById(R.id.player_text);
-        TextView score = view.findViewById(R.id.score_text);
-
-        playerName.setText(leaderboardContent.getUserName());
-        score.setText(""+ leaderboardContent.getScore());
-
-
+        // get database information
+//        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//        CollectionReference collectionReference = db.collection(Player.class.getSimpleName());
+//        Intent myIntent = new Intent(PlayerProfileViewActivity.this, ProfileQRListViewActivity.class);
+//        myIntent.putExtra("android_id", android_id);
+//        // put data into class
+//        db.collection(Player.class.getSimpleName()).document(android_id).get()
+//                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                        player = new Player((String)documentSnapshot.get("username"), (String)documentSnapshot.get("deviceID"), (ArrayList<QR>) documentSnapshot.get("qrList"),
+//                                (int) Integer.parseInt((String)documentSnapshot.get("rank")), (int) Integer.parseInt((String)documentSnapshot.get("highestScore")),
+//                                (int)Integer.parseInt((String)documentSnapshot.get("lowestScore")), (int)Integer.parseInt((String)documentSnapshot.get("totalScore")));
+//
+//                        // update UI
+//                        usernameTextView.setText(player.getUsername());
+//                        totalScoreTextView.setText("Total Score: " + player.getTotalScore());
+//                        totalScannedTextView.setText("Total Scanned: " + player.getTotalQR());
+//                    }
+//
+//
         return view;
+//    }
     }
 
+    /**
+     *
+     * @param charText: user's input into search bar
+     * Filters out content on the leaderboard based on the user's input
+     */
     public void filter(String charText) {
         charText = charText.toLowerCase(Locale.getDefault());
         leaderboardContentList.clear();
