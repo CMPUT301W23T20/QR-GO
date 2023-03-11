d8b798e295a76abd77e1c635cf1ab9931c355403
vanessahuang13 <vchuang@ualberta.ca>
2023-03-11 16:05:00 -0700

    added javadocs for camera and location activity

--- app/src/main/java/com/example/qr_go/Activities/Scan/CameraActivity.java
+++ app/src/main/java/com/example/qr_go/Activities/Scan/CameraActivity.java
@@ -13,10 +13,27 @@ import android.widget.Toast;
 
 import com.example.qr_go.R;
 
+//From Youtube.com
+// URL: https://www.youtube.com/watch?v=59taMJThsFU
+// Author: https://www.youtube.com/@allcodingtutorials1857
+
+
+/**
+ * This class displays the activity which gives the player the option to record a photo
+ *
+ */
 public class CameraActivity extends AppCompatActivity {
     private static final int REQUEST_CODE = 22;
     Button btnpicture;
 
+    /**
+     * This function is called right when the activity starts
+     * It ensures that when the button for the camera is clicked, it will show a camera
+     * @param savedInstanceS state If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +49,26 @@ public class CameraActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * This function is called after a picture is taken
+     * It creates a Bitmap object which is the photo, this will be stored with the QR code
+     * It then starts the next activity, which is GetLocationActivity
+     *
+     * @param requestCode The integer request code originally supplied to
+     *                    startActivityForResult(), allowing you to identify who this
+     *                    result came from.
+     * @param resultCode The integer result code returned by the child activity
+     *                   through its setResult().
+     * @param data An Intent, which can return result data to the caller
+     *               (various data can be attached to Intent "extras").
+     *
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode ==REQUEST_CODE && resultCode == RESULT_OK)
         {
             Bitmap photo = (Bitmap)data.getExtras().get("data");
+            //pass photo to be stored here
             Intent locationIntent = new Intent(CameraActivity.this, GetLocationActivity.class);
             startActivity(locationIntent);
         }
