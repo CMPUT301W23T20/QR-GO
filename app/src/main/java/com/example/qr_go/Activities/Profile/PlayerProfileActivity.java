d691f0e143f532b3fb086572521af6745a5b003b
ethantrac <etrac@ualberta.ca>
2023-03-30 20:11:16 -0600

    View other profiles implemented

--- app/src/main/java/com/example/qr_go/Activities/Profile/PlayerProfileActivity.java
+++ app/src/main/java/com/example/qr_go/Activities/Profile/PlayerProfileActivity.java
@@ -1,27 +1,18 @@
-package com.example.qr_go.Fragments.Profile;
+package com.example.qr_go.Activities.Profile;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.qr_go.Activities.Profile.OtherProfileQRListViewActivity;
-import com.example.qr_go.Activities.Profile.ThisProfileQRListViewActivity;
+import com.example.qr_go.Activities.QRView.QRViewActivity;
 import com.example.qr_go.Actor.Player;
 import com.example.qr_go.DataBaseHelper;
-import com.example.qr_go.MainActivity;
 import com.example.qr_go.QR.QR;
 import com.example.qr_go.R;
-
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
-
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -29,110 +20,78 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Represents a player's profile
- */
-public class PlayerProfileFragment extends Fragment {
+public class PlayerProfileActivity extends ProfileActivity {
+
     private TextView usernameTextView;
     private TextView contactTextView;
     private Button qrButton;
+    private Button backButton;
     private TextView totalScoreTextView;
     private TextView totalScannedTextView;
-    private final String android_id;
-    private final boolean isThisDevice;
     private Player model;
     private View view;
     private String test;
     private DataBaseHelper dbHelper = new DataBaseHelper();
 
-    private MainActivity mainActivity;
+    public PlayerProfileActivity() {
 
-    /**
-     * Constructor
-     * @param android_id
-     * Android ID of player
-     * @param isThisDevice
-     * If the android ID belongs to player on this device
-     */
-    public PlayerProfileFragment(String android_id, boolean isThisDevice) {
-        this.android_id = android_id;
-        this.isThisDevice = isThisDevice;
-    }
-
-    public static Fragment newInstance(String android_id, boolean isThisDevice) {
-        PlayerProfileFragment fragment = new PlayerProfileFragment(android_id, isThisDevice);
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
+        setContentView(R.layout.activity_player_profile);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_player_profile, container, false);
+        getViews();
 
+        try {
+            getIDFromBundle();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-        getViews(view);
+        updateProfileInfo();
 
         qrButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent myIntent;
+                Bundle extras = getIntent().getExtras();
+                Boolean isThisDevice = extras.getBoolean("isThisDevice");
                 // allow QR deletion if profile is this player
                 if(isThisDevice) {
-                    myIntent = new Intent(getActivity(), ThisProfileQRListViewActivity.class);
+                    myIntent = new Intent(PlayerProfileActivity.this, ThisProfileQRListViewActivity.class);
                 }
                 // don't allow QR deletion if profile is other player
                 else {
-                    myIntent = new Intent(getActivity(), OtherProfileQRListViewActivity.class);
+                    myIntent = new Intent(PlayerProfileActivity.this, OtherProfileQRListViewActivity.class);
                 }
                 myIntent.putExtra("android_id", android_id);
-                startActivity(myIntent);
 
+                PlayerProfileActivity.this.startActivity(myIntent);
             }
         });
 
-        // Inflate the layout for this fragment
-        return view;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        View view = getView();
-        updateProfileInfo(view);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        View view = getView();
-
-        // only call if data changed
-        // to be implemented
-        updateProfileInfo(view);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     /**
      * Updates the profile information on screen
      */
-    private void updateProfileInfo(View view) {
-
-        getViews(view);
+    private void updateProfileInfo() {
 
 
-         // get database information
-         FirebaseFirestore db = FirebaseFirestore.getInstance();
-         CollectionReference collectionReference = db.collection(Player.class.getSimpleName());
+        // get database information
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = db.collection(Player.class.getSimpleName());
 
-         // put data into class
+        // put data into class
         db.collection(Player.class.getSimpleName()).document(android_id).get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
@@ -163,12 +122,13 @@ public class PlayerProfileFragment extends Fragment {
     /**
      * Gets views from fragment
      */
-    public void getViews(View view) {
+    public void getViews() {
         // get views from fragment
-        usernameTextView = view.findViewById(R.id.username_text);
-        qrButton = view.findViewById(R.id.my_qr_codes_button);
-        totalScoreTextView = view.findViewById(R.id.total_score_text);
-        totalScannedTextView = view.findViewById(R.id.total_scanned_text);
-        contactTextView = view.findViewById(R.id.contact_text);
+        usernameTextView = findViewById(R.id.username_text);
+        qrButton = findViewById(R.id.my_qr_codes_button);
+        totalScoreTextView = findViewById(R.id.total_score_text);
+        totalScannedTextView = findViewById(R.id.total_scanned_text);
+        contactTextView = findViewById(R.id.contact_text);
+        backButton = findViewById(R.id.back_button);
     }
 }
