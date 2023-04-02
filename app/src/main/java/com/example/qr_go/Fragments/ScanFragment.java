ca1f1cd72f33188dba6f6c88003052b85cc3dd00
ethantrac <etrac@ualberta.ca>
2023-04-01 23:44:51 -0600

    update player list

--- app/src/main/java/com/example/qr_go/Fragments/ScanFragment.java
+++ app/src/main/java/com/example/qr_go/Fragments/ScanFragment.java
@@ -19,17 +19,22 @@ import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import com.example.qr_go.Activities.Profile.GreetingScreenActivity;
 import com.example.qr_go.Activities.Scan.CameraActivity;
 import com.example.qr_go.Activities.Scan.CaptureAct;
 import com.example.qr_go.Actor.Player;
 import com.example.qr_go.DataBaseHelper;
+import com.example.qr_go.MainActivity;
 import com.example.qr_go.QR.QR;
 import com.example.qr_go.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -212,7 +217,26 @@ public class ScanFragment extends Fragment {
 
                             // update DB
                             dbHelper.updateDB(player);
-                            dbHelper.updateDB(qr);
+
+                            db.collection(QR.class.getSimpleName()).document(qr.getQrHash()).get()
+                                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                            if(!task.getResult().exists()) {
+                                                dbHelper.updateDB(qr);
+                                            }
+                                            else {
+                                                HashMap<String, Object> data = new HashMap<>();
+
+                                                List<String> playerList = (List<String>) documentSnapshot.get("playerList");
+                                                playerList.add(player.getDeviceID());
+
+                                                data.put("playerList", data);
+
+                                                db.collection(QR.class.getSimpleName()).document(qr.getQrHash()).update(data);
+                                            }
+                                        }
+                                    });
                         }
                     });
 
