e3b08573cad500081b56c082cdf788ffe3f28f65
Rex <yongxin1@ualberta.ca>
2023-04-02 23:15:21 -0600

    comment added

--- app/src/main/java/com/example/qr_go/Coupon.java
+++ app/src/main/java/com/example/qr_go/Coupon.java
@@ -6,17 +6,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * This class generate coupon
+ */
 public class Coupon {
     private ArrayList<String> dataList = new ArrayList<>();
-    private final String gl = "Good lyck next time!! Deep scanning for a discount code!!!";
+    private final String gl = "Good luck next time!! Deep scanning for a discount code!!!";
     private final String cg = "Congratulation!! You get a free coffee!!! Contact the team with following coupon: ";
 
+    /**
+     * Coupon constructor
+     */
     public Coupon() {
         this.dataList = dataList;
         String[] code = {"CEK6GCN", "CEK6GCN", "N17ATBP", "29K6TBN", "QAXWGCU", "QQYJMGN"};
         dataList.addAll(Arrays.asList(code));
     }
 
+    /**
+     * this return a string with a coupon
+     * @return
+     * String
+     */
     public String lottery(){
         Random randI = new Random();
         int i = randI.nextInt(10);
