f2fb76e2c4370139a8b3575a0de369601bbb322d
u-nis <you-nis@outlook.com>
2023-04-02 23:18:12 -0600

    added javadocs for avatar class

--- app/src/main/java/com/example/qr_go/QR/Avatar.java
+++ app/src/main/java/com/example/qr_go/QR/Avatar.java
@@ -1,6 +1,9 @@
 package com.example.qr_go.QR;
 
 public class Avatar {
+    /**
+     * An array of strings representing different face shapes.
+     */
     private static final String[] FACE_SHAPES = {
             "  ( o_O )   ",
             "  ( o.o )  ",
@@ -14,6 +17,9 @@ public class Avatar {
             "  ( >_< )   ",
             "  (* > *)  "
     };
+    /**
+     * An array of strings representing different body features.
+     */
 
     private static final String[] BODY_FEATURES = {
             "  |   |  ",
@@ -28,7 +34,9 @@ public class Avatar {
             "  / o \\  ",
             "  ) . (  "
     };
-
+    /**
+     * An array of strings representing different leg features.
+     */
     private static final String[] LEG_FEATURES = {
             "/   \\",
             "|   |",
@@ -42,7 +50,9 @@ public class Avatar {
             "|/|\\|",
             "|>|<|"
     };
-
+    /**
+     * An array of strings representing different hat features.
+     */
     private static final String[] HAT_FEATURES = {
             "   ___   ",
             "  /   \\  ",
@@ -57,7 +67,9 @@ public class Avatar {
             "  |_|_|_|  ",
             "  ooooo  "
     };
-
+    /**
+     * An array of strings representing different shoe features.
+     */
     private static final String[] SHOE_FEATURES = {
             "<_|_>",
             ">_|_<",
@@ -68,6 +80,11 @@ public class Avatar {
             "....."
     };
 
+    /**
+     * Generates an avatar based on a given hash.
+     * @param hash a string representing the hash to use to generate the avatar
+     * @return a string representing the generated avatar
+     */
     public String generateAvatar(String hash) {
         String hex = hash.substring(0, 10);
 
@@ -95,6 +112,12 @@ public class Avatar {
         return avatar;
     }
 
+    /**
+     * Pads a given string with spaces to a given length.
+     * @param str the string to pad
+     * @param padding the number of spaces to add to the left and right of the string
+     * @return a padded string
+     */
     private String padString(String str, int padding) {
         return " ".repeat(Math.max(0, padding)) + str;
     }
